b00dfb839787bbef9797daebb15f2e3637aaf787
Filip Chalupa <chalupa.filip@gmail.com>
2024-02-26 18:00:22 +0100

    WIP

--- src/createQueue.ts
+++ src/createQueue.ts
@@ -3,12 +3,12 @@ export type Task<ReturnValue> = () => Promise<ReturnValue>
 export const createQueue = (): {
 	enqueueTask: <ReturnValue>(task: Task<ReturnValue>) => Promise<ReturnValue>
 } => {
-	type Item<ReturnValue = unknown> = {
+	type Item<ReturnValue> = {
 		task: Task<ReturnValue>
 		markDone: (returnValue: ReturnValue) => void
 	}
 	// @TODO: add discardPendingTasks function
-	const items: Item[] = []
+	const items: Item<unknown>[] = []
 	const enqueueTask = <ReturnValue>(
 		task: Task<ReturnValue>,
 	): Promise<ReturnValue> => {
@@ -43,3 +43,26 @@ export const createQueue = (): {
 
 	return { enqueueTask }
 }
+
+const q = createQueue()
+
+const task1 = async () => {
+	return 5
+}
+const task2 = async () => {
+	return 'asda'
+}
+const task3 = async () => {
+	return new Promise<string>((resolve) => {
+		resolve('Míša')
+	})
+}
+
+const result1 = q.enqueueTask(task1)
+console.log(result1)
+
+const result2 = q.enqueueTask(task2)
+console.log(result2)
+
+const result3 = q.enqueueTask(task3)
+console.log(result3)
