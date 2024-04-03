be71545d7da306700830f652481c1f8e66db844b
Filip Chalupa <chalupa.filip@gmail.com>
2024-04-03 14:03:49 +0200

    Add basic test

---
+++ test/run.js
@@ -0,0 +1,33 @@
+import { queue } from '../dist/index.js'
+
+const delay = (milliseconds) =>
+	new Promise((resolve) => setTimeout(resolve, milliseconds))
+
+const log = (message) => {
+	const now = new Date()
+	console.log(
+		`[${now.toLocaleTimeString('en-GB', {
+			hour: '2-digit',
+			minute: '2-digit',
+			second: '2-digit',
+		})}.${now.getMilliseconds().toString().padStart(3, '0')}]`,
+		message,
+	)
+}
+
+const task1 = async () => {
+	await delay(500)
+	log('Task 1')
+}
+const task2 = async () => {
+	await delay(350)
+	log('Task 2')
+}
+const task3 = async () => {
+	await delay(400)
+	log('Task 3')
+}
+
+queue.enqueueTask(task1)
+queue.enqueueTask(task2)
+queue.enqueueTask(task3)
